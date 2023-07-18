f459b42f689603d1a2653161e9acb0c8b6259e8b
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-07-18 16:48:19 -0300

    first commit

--- src/main/java/ar/agenda/agendaVirtual/controller/ControllerContacto.java
+++ src/main/java/ar/agenda/agendaVirtual/controller/ControllerContacto.java
@@ -3,6 +3,7 @@ package ar.agenda.agendaVirtual.controller;
 import ar.agenda.agendaVirtual.model.Contacto;
 import ar.agenda.agendaVirtual.repository.RepositoryContacto;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,6 +45,41 @@ public class ControllerContacto {
         contacto.setFecharegistro(LocalDateTime.now()); //esto lo que hace es traernos la fecha y hora actual y lo asigna a fecharegistro
         contactoRepository.save(contacto);
         ra.addFlashAttribute("msgExito", "El contacto " + contacto.getNombre() + " se ah creado correctamente");
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/editar")
+    String editar(@PathVariable Integer id, Model model) {
+        Contacto contacto = contactoRepository.getById(id);
+
+        // Formateamos la fecha de nacimiento como una cadena en formato "yyyy-MM-dd"
+        String fechaNacimientoFormatted = contacto.getFechanac().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        model.addAttribute("contacto", contacto);
+        model.addAttribute("fechaNacimientoFormatted", fechaNacimientoFormatted);
+        return "nuevo";
+    }
+
+    @PostMapping("/{id}/editar") //aca editamos al contacto
+    String actualizar(@PathVariable Integer id,
+            @Validated Contacto contacto,
+            BindingResult bindingResult,
+            RedirectAttributes ra,
+            Model model
+    ) { //Vamos a utilizar redirecattribute que lo que hace es poder mostrar mensajes como de error o de exito.
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("contacto", contacto);
+            return "nuevo";
+        }
+
+        Contacto contactoFromDb = contactoRepository.getById(id);
+        contactoFromDb.setNombre(contacto.getNombre());
+        contactoFromDb.setCelular(contacto.getCelular());
+        contactoFromDb.setEmail(contacto.getEmail());
+        contactoFromDb.setFechanac(contacto.getFechanac());
+
+        contactoRepository.save(contactoFromDb); //Save hace 2 cosas: crea y actualiza, se basa en el id, si el objeto tiene un id se actualiza, si no se crea el objeto.
+        ra.addFlashAttribute("msgExito", "El contacto " + contacto.getNombre() + " se ah actualizado correctamente");
         return "redirect:/";
     }
 
